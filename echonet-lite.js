790d5dec642c08db374928375906582769670a85
yaoyu <yaoyuli@pqdusa.com>
2023-12-25 11:34:36 +0800

    1. replace node with ctx

--- echonet-lite.js
+++ echonet-lite.js
@@ -1,25 +1,42 @@
-var EchonetLite = require("node-echonet-lite");
+var ctx = null;
+
 module.exports = function (RED) {
+  var EchonetLite = require("node-echonet-lite");
+  var el = new EchonetLite({ type: "lan" });
   // echonet-lite node
   function EchonetLiteNode(config) {
     RED.nodes.createNode(this, config);
+
+    el.init((err) => {
+      if (err) {
+        showErrorExit(err);
+      } else {
+        this.status({ fill: "green", shape: "dot", text: "connected" });
+        // discoverDevices();
+      }
+    });
+
+    // el.on("notify", (res) => {
+    //   this.status({ fill: "orange", shape: "dot", text: "updated" });
+    //   console.log(
+    //     "[NOTIFY] From: " + res["device"]["address"] + " --------------------------"
+    //   );
+    //   console.log(JSON.stringify(res["message"], null, "  "));
+    //   console.log("");
+    // });
+
     const node = this;
+    ctx = this;
     node.location = config.location;
-    node.el = new EchonetLite({ type: "lan" });
     node.on("input", function (msg) {
       let location = node.location ? node.location : msg.payload.location;
       if (!location) {
         node.error("missing location", msg);
       } else {
-        node.el.init((err) => {
-          if (err) {
-            showErrorExit(err);
-          } else {
-            discoverDevices(node);
-          }
-        });
+        discoverDevices();
+
         msg.payload = {
-          text: `msg has ${location} original payload is ${msg.payload}`,
+          text: `loc 2 ${location} original payload is ${msg.payload}`,
         };
         node.send(msg);
       }
@@ -29,9 +46,9 @@ module.exports = function (RED) {
   RED.nodes.registerType("echonet-lite", EchonetLiteNode, {});
 
   // Start to discover devices
-  function discoverDevices(node) {
+  function discoverDevices() {
     // Start to discover Echonet Lite devices
-    node.el.startDiscovery((err, res) => {
+    el.startDiscovery((err, res) => {
       // Error handling
       if (err) {
         showErrorExit(err);
@@ -50,12 +67,12 @@ module.exports = function (RED) {
       console.log(`Found ${JSON.stringify(device)} at ${address}`);
       if (group_code === 0x02 && class_code === 0x7c) {
         // Stop to discovery process
-        node.el.stopDiscovery();
+        el.stopDiscovery();
         // This means that the found device belongs to the home air conditioner class
         // Get the operation status
-        getOperationStatus(address, eoj, node);
+        getOperationStatus(address, eoj);
 
-        node.el.getPropertyValue(address, eoj, 0xbb, (err, res) => {
+        el.getPropertyValue(address, eoj, 0xbb, (err, res) => {
           console.log("- Property value:");
           console.dir(res["message"]["data"]);
         });
@@ -64,22 +81,24 @@ module.exports = function (RED) {
   }
 
   // Get the operation status
-  function getOperationStatus(address, eoj, node) {
+  function getOperationStatus(address, eoj) {
     var epc = 0x80; // An property code which means the operation status
-    node.el.getPropertyValue(address, eoj, epc, (err, res) => {
+    ctx.status({ fill: "yellow", shape: "dot", text: "getting" });
+    el.getPropertyValue(address, eoj, epc, (err, res) => {
       // this value is true if the air conditione is on
       var status = res["message"]["data"]["status"];
       var desc = status ? "on" : "off";
       console.log("The fuel cell is " + desc + ".");
+      ctx.status({ fill: "blue", shape: "dot", text: "got" });
       // Toggle the status of the operation status
-      changePowerStatus(address, eoj, epc, !status, node);
+      changePowerStatus(address, eoj, epc, !status);
     });
   }
 
   // Change the status of the operation status
-  function changePowerStatus(address, eoj, epc, status, node) {
+  function changePowerStatus(address, eoj, epc, status) {
     var edt = { status: status };
-    node.el.setPropertyValue(address, eoj, epc, edt, (err, res) => {
+    el.setPropertyValue(address, eoj, epc, edt, (err, res) => {
       var desc = status ? "on" : "off";
       console.log("The air conditionaer was turned " + desc + ".");
       // node.el.close(() => {
